4477cb13b6bcc000207fdbff88c1eb3becf23e4a
ztg65f <102537310+ztg65f@users.noreply.github.com>
2026-05-07 19:54:23 +0200

    Set decoration scale to 1.8

--- components/AvatarWithDecoration.tsx
+++ components/AvatarWithDecoration.tsx
@@ -9,7 +9,7 @@ interface Props {
   onClick?: () => void
 }
 
-const SCALE = 1.7
+const SCALE = 1.8
 
 export default function AvatarWithDecoration({ avatarUrl, displayInitial, size, decoration, className = '', onClick }: Props) {
   const dec     = decorationById(decoration)
